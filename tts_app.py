98d05c79dd94d820aa0aa82ee04572328c1b0e1b
Mert <62549656+fswair@users.noreply.github.com>
2025-02-24 19:29:00 +0300

    v1

---
+++ tts_app.py
@@ -0,0 +1,31 @@
+import torch
+from TTS.api import TTS
+from pydantic import BaseModel
+
+class VoiceGen(BaseModel):
+    speaker : str
+    tts_model: str
+    language: str = "en"
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    @property
+    def tts(self):
+        return TTS(self.tts_model).to(self.device)
+    
+    def to_file(self, text: str, dummy_params: tuple[str], output_path: str, split_sentences: bool = None, **options):
+        head, tail = dummy_params[:2]
+        text = f"{head} {text} {tail}"
+        return self.tts.tts_to_file(text=text, speaker_wav=self.speaker, language=self.language, file_path=output_path, split_sentences=bool(split_sentences), **options)
+
+tts = VoiceGen(
+    speaker="tts/tests/data/ljspeech/wavs/LJ001-0001.mp3",
+    tts_model="tts_models/multilingual/multi-dataset/xtts_v2",
+    language="tr"
+)
+
+tts.to_file(
+    text="kırmızı",
+    dummy_params=("istanbul", "ankara"),
+    output_path="output.wav",
+    split_sentences=True
+)
